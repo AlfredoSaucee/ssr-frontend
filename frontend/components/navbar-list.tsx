528a0ca35ecb1ad376860103386a90e13939954f
AlfredoSaucee <almquist.alfred@gmail.com>
2025-09-18 21:49:06 +0200

    Added logo

--- frontend/components/navbar-list.tsx
+++ frontend/components/navbar-list.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 const navbarItems = () => {
   return (
-    <div className='flex items-center gap-4 px-4 py-2 rounded justify-between w-full'>
-        <Link href="/" className='font-bold text-white text-2xl'>SSR-EDITOR</Link>
+    <div className='flex items-center gap-4 px-4  rounded justify-between w-full'>
+        <Image src="/noetly.svg" alt="logo" width={140} height={140} className="fill-white"/>
         <Link href="/dokument">Dokument</Link>
       
     </div>
